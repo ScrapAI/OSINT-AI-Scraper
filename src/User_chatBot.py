417184d5cb0049c1865ecd8a1c589223647d3a2c
sebDtSci <sebastientadiello@gmail.com>
2025-01-19 23:35:27 +0100

    new archi

--- src/User_chatBot.py
+++ src/User_chatBot.py
@@ -1,8 +1,12 @@
-from langchain_ollama.llms import OllamaLLM
-from langchain.memory import ConversationSummaryMemory
-from src.rag.Rag import Rag
-# from langchain.llms.base import BaseLanguageModel
-from src.fc.func import get_weather
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import ollama
+import logging
+import os
+from shortterm_memory.ChatbotMemory import ChatbotMemory
+from rag.Rag import Rag
+from fc.func import get_weather
 
 functions_registry = {
     "get_weather": {
@@ -16,22 +20,19 @@ functions_registry = {
 
 def detect_function(input_text: str):
     if "météo" in input_text.lower():
-        return "get_weather", {"city": input_text.split()[-1]}  # Exemple basique
+        return "get_weather", {"city": input_text.split()[-1]} 
     return None, {}
 
 class User_chatBot:
-    def __init__(self, model:str = "mistral:latest", ollama_options:dict = None):
-        
-        self.ollama_model = OllamaLLM(
-            model=model,
-            options=ollama_options if ollama_options else {'temperature': 1} # vérifier le paramétrage de la température 0 pour déterministe langchain et 1 pour ollama_python
-            )
-        self.output = ""
-        ConversationSummaryMemory.model_rebuild()
-        self.history = ConversationSummaryMemory(llm=self.ollama_model)
+    def __init__(self, model:str="mistral:latest", ollama_options=None):
+        self.model = model
+        self._ollama_option = ollama_options if ollama_options else {'temperature': 1} # vérifier le paramétrage de la température 0 pour déterministe langchain et 1 pour ollama_python
+        self.memory = ChatbotMemory()
         self.running = False
+        self.response = ""
+        self.suma_on_run = False
         self.rag = Rag()
-        
+    
     def ans(self, input: str):
         prompt: str
         context: str
@@ -43,15 +44,16 @@ class User_chatBot:
             try:
                 func = functions_registry[func_name]['func']
                 result = func(**params)
-                self.output = result
-                yield result
+                input += (f" \n voici les resultas fournit par l'outil {func_name} : "+result)
+                # self.output = result
+                # yield result
             except Exception as e:
                 self.output = f"Erreur lors de l'appel de la fonction {func_name} : {str(e)}"
                 yield self.output
             return 0
 
         try:
-            mem = self.history.load_memory_variables({}).get('history', "")
+            mem = self.memory.get_memory()
         except Exception as e:
             self.output = f"Erreur lors de l'appel de la fonction Memoire : {str(e)}"
             mem = ""
